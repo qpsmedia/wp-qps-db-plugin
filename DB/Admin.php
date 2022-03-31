46a694019b2cd2df8e587e14a165c8ff8a5011e5
flynsarmy <flynsarmy@gmail.com>
2022-03-31 11:36:44 +1000

    Disable cache on status AJAX request

--- DB/Admin.php
+++ DB/Admin.php
@@ -70,6 +70,13 @@ class Admin
     public function ajaxYTGetStatus()
     {
         header('Content-Type: application/json');
+        // Disable cache
+        header("Cache-Control: no-store, no-cache, must-revalidate"); // HTTP/1.1
+        header("Cache-Control: post-check=0, pre-check=0", false);
+        header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+        header("Pragma: no-cache"); // HTTP/1.0
+        header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+
         $id = intval(Helpers::GET('id'));
         $post = get_post($id);
 
